852361cfb43134205b3fd127e82c60687b331119
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-03-27 19:34:57 +0200

    Add the beginning of a strategy
    Need to test the optimal value and the comparisons.

--- src/strategy/greedy.rs
+++ src/strategy/greedy.rs
@@ -16,6 +16,36 @@ impl fmt::Display for Greedy {
 
 impl Strategy for Greedy {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        unimplemented!("TODO: algo glouton")
+        if state.movements().next().is_some() {
+            println!("current_player is : {:?}", state.current_player);
+            let mut optimal_value: i8;
+            if state.current_player {
+                // Blue need to maximaxe the value
+                optimal_value = -128;
+            } else {
+                // Red need to minimize it
+                optimal_value = 127;
+            }
+            let mut optimal_mouvement = state.movements().next();
+            for mouv in state.movements() {
+                let new_value = state.play(&mouv).value();
+                println!("Value : {}, Mouvement : {:?}", new_value, mouv);
+                if state.current_player {
+                    if new_value > optimal_value {
+                        optimal_mouvement = Some(mouv);
+                        optimal_value = new_value;
+                    }
+                } else {
+                    if new_value > optimal_value {
+                        optimal_mouvement = Some(mouv);
+                        optimal_value = new_value;
+                    }
+                }
+            }
+            println!("We have taken : {:?}", optimal_mouvement);
+            return optimal_mouvement;
+        } else {
+            return None;
+        }
     }
 }
