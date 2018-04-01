2b079ab6d070927a9051fd0acc49931857579cd6
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-04-01 17:31:40 +0200

    Patch the files
    Apply blobwar.patch, using:
    patch -p1 < blobwar.patch

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,7 @@
 extern crate blobwar;
 use blobwar::configuration::Configuration;
 use blobwar::board::Board;
-use blobwar::strategy::{IterativeDeepening, IterativeStrategy, NetworkPlayer};
+use blobwar::strategy::{AlphaBeta, NetworkPlayer};
 
 use std::net::TcpListener;
 use std::env::args;
@@ -17,8 +17,5 @@ fn main() {
     // create board and start game
     let board = Board::load(map_name).expect("failed loading map");
     let mut game = Configuration::new(&board);
-    game.battle(
-        IterativeDeepening::new(IterativeStrategy::AlphaBeta),
-        players.next().unwrap(),
-    );
+    game.battle(AlphaBeta(6), players.next().unwrap());
 }
