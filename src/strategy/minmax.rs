c175e00c570734d457a2e34eabdcb4359479cf4e
Yohan GOUZERH <yohan.gouzerh@outlook.com>
2018-04-07 16:01:33 +0200

    Add minmax algorithm (#4)
    * Add MinMax dirty strategy
    
    This work only for the red player and
    is proved to work with depth 3 only
    Possible improvements:
        - Work for all depth (the -self.value() is not a good solution)
        - Independent of the player color
        - Transformation into negamax algorithm
    
    * Work with all depths
    
    But not with all player positions.
    TODO : need to beed independent.
    
    * Add MinMax algorithm
    
    It has been tested for the two players and work with all
    depth.
    The correction was done by invert the value at each recursion,
    because the next level of the recursion give an inversed value.

--- src/strategy/minmax.rs
+++ src/strategy/minmax.rs
@@ -7,9 +7,40 @@ use shmem::AtomicMove;
 /// Min-Max algorithm with a given recursion depth.
 pub struct MinMax(pub u8);
 
+// It's more a negamax here than a min max algorithm, due to the inversion done by state.play()
+fn minmax(state: &Configuration, depth: i8, is_the_player: bool) -> (Option<Movement>, i8) {
+    // Test if we have a movement to perform or if we aren't at the leaves
+    if state.movements().next().is_some() && depth > 0 {
+        let mut best_value: i8 = 127;
+        let mut best_movement: Option<Movement> = None;
+        // For each possible movements we test he possible combinations
+        for mov in state.movements() {
+            let (_, v_read) = minmax(&state.play(&mov), depth - 1, is_the_player);
+            // We invert the value at each turn because the second level will give the value
+            // for himslef
+            let value = -v_read;
+            // Keep if it's a better value
+            if value < best_value {
+                best_value = value;
+                best_movement = Some(mov);
+            }
+        }
+        return (best_movement, best_value);
+    } else {
+        // The last level return the value of th egame
+        return (None, state.value());
+    }
+}
+
 impl Strategy for MinMax {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        unimplemented!("TODO: implementer min max")
+        println!(
+            "MINMAX me : current_player = {:?} , value = {:?}",
+            state.current_player,
+            state.value()
+        );
+        let (best_movement, _) = minmax(state, 2, state.current_player);
+        best_movement
     }
 }
 
