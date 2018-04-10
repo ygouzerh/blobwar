b5dd06597fd41f328b406472ea92f58cd662cd8f
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-04-10 10:12:24 +0200

    Fix some scripts files
    There was a duplication between the two files.

--- run_deep_main.sh
+++ run_deep_main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Script to run automatically without optimization the main with deepening
-export PATH="$(pwd)/target/release":$PATH
+export PATH=$(pwd)/target/release:$PATH
 cargo build --release --bin blobwar_iterative_deepening
 cargo run --release --bin blobwar
--- run_main_deepening.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script to run automatically without optimization the main with deepening
-export PATH=$(pwd)/target/release:$PATH
-cargo build --release --bin blobwar_iterative_deepening
-cargo run --release --bin blobwar
